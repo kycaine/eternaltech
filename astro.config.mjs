900abfc7f0741b35c2b4d79827441d254cec5c2c
kycaine <rizkie.kudo@gmail.com>
2026-02-24 03:59:12 +0800

    update ppath bg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
-
 import { defineConfig } from 'astro/config';
-import github from '@astrojs/github';
 
 export default defineConfig({
-	output: 'static',
-	adapter: github(),
-	site: 'https://user.github.io/eternaltech', // Ganti 'user' dengan username GitHub kamu
-	base: '/eternaltech/',
+  output: 'static',
+  site: 'https://kycaine.github.io',
+  base: '/eternaltech/',
 });
